8535c7dbc9f91a8f5ff037f82e7af41a92a57fbb
yanzt <yanzt@gistack.cn>
2021-07-09 23:11:34 +0800

    feat: read json file from local assets

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by zhitao on 2021/7/9.
+//
+
+import Foundation
+
+var landmarks: [Landmark]  = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+            else {
+        fatalError("couldn't find  \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("couldn't load no \(filename) in main bundle.")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
