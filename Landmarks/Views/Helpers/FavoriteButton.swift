c0d8715541cf923d7bf8f773d91db978741ba214
yanzt <yanzt@gistack.cn>
2021-07-10 11:32:49 +0800

    feat: User can mark landmark as the favorite.

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by zhitao on 2021/7/10.
+//
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+      Button(action: {isSet.toggle()}){
+          Image(systemName: isSet ? "star.fill": "star")
+          .foregroundColor(isSet ? Color.yellow : Color.gray)
+      }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
